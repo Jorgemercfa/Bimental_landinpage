3870f766820d5457eb3173d3fb3f0dd8417e9f9e
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2025-05-03 23:36:32 -0500

    create_desing_home_view

--- src/views/Home-item.vue
+++ src/views/Home-item.vue
@@ -7,9 +7,14 @@ import Footer from '@/components/Footer-item.vue';
   <header>
     <navbar />
   </header>
+  <h1 class="title-home">Bimental</h1>
+  <h3 class="subtitle-home">Evalúa tu salud mental en minutos</h3>
   <div class="home-area">
-    <h1>Bimental</h1>
     <img class="home-image" src="@/assets/homeimg.png" />
+    <h3 class="home-description">
+      Bimental es una app que te ayuda a identificar niveles de ansiedad,
+      depresión y estrés mediante un chatbot inteligente
+    </h3>
   </div>
   <footer>
     <Footer />
@@ -26,22 +31,38 @@ import Footer from '@/components/Footer-item.vue';
   align-items: center;
 }
 
+.title-home {
+  font-size: 84px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.subtitle-home {
+  font-size: 24px;
+  margin-bottom: 2px;
+  text-align: center;
+}
+
+.home-description {
+  font-size: 24px;
+  margin-top: 100px;
+}
+
 .home-image {
-  width: 600px;
-  height: 600px;
+  height: 400px;
   border-radius: 10px;
   align-items: center;
 }
 
 @media (min-width: 768px) and (max-width: 1024px) {
   .home-image {
-    height: 500px;
+    height: 300px;
   }
 }
 
 @media (min-width: 300px) and (max-width: 768px) {
   .home-image {
-    height: 400px;
+    height: 200px;
   }
 }
 </style>
